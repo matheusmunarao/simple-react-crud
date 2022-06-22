cabdc5fb7dd8e39f32f6934eceff6d21e8e3d70f
Matheus Munarão <matheusmunarao1@gmail.com>
2022-06-22 19:29:02 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import Update from "./pages/update/Update.js";
 
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
+import { Amplify } from "aws-amplify";
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig);
+
 const App = () => {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { Amplify } from "aws-amplify";
-import awsconfig from './aws-exports'
-Amplify.configure(awsconfig);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
